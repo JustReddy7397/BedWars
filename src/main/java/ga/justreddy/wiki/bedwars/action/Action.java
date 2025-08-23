cf578dd7fe2dc9ea557babe3be1c5abe39f3a95a
JustReddy7397 <novoiceover2@gmail.com>
2025-08-23 22:04:36 +0200

    Add actions

---
+++ src/main/java/ga/justreddy/wiki/bedwars/action/Action.java
@@ -0,0 +1,15 @@
+package ga.justreddy.wiki.bedwars.action;
+
+import ga.justreddy.wiki.bedwars.BedWars;
+import ga.justreddy.wiki.bedwars.model.entity.GamePlayer;
+
+/**
+ * @author JustReddy
+ */
+public abstract class Action {
+
+    public abstract String getId();
+
+    public abstract void onAction(BedWars bedWars, GamePlayer gamePlayer, String data);
+
+}
---
+++ src/main/java/ga/justreddy/wiki/bedwars/action/ActionManager.java
@@ -0,0 +1,65 @@
+package ga.justreddy.wiki.bedwars.action;
+
+import ga.justreddy.wiki.bedwars.BedWars;
+import ga.justreddy.wiki.bedwars.model.entity.GamePlayer;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * @author JustReddy
+ */
+public class ActionManager {
+
+    private static boolean initialized = false;
+
+    private final Map<String, Action> actions;
+    private final BedWars plugin;
+
+    public ActionManager(BedWars plugin) {
+        if (initialized) {
+            throw new IllegalStateException("ActionManager is already initialized.");
+        }
+        this.plugin = plugin;
+        this.actions = new HashMap<>();
+        initialized = true;
+    }
+
+    private void registerActions(Action... actions) {
+        for (final Action action : actions) {
+            if (this.actions.containsKey(action.getId())) {
+                throw new IllegalArgumentException("Action with ID '" + action.getId() + "' is already registered.");
+            }
+            this.actions.put(action.getId(), action);
+        }
+    }
+
+    public void onAction(Player player, List<String> actions) {
+        Optional<GamePlayer> optionalGamePlayer = GamePlayer.get(player);
+        if (optionalGamePlayer.isEmpty()) {
+            return;
+        }
+
+        final GamePlayer gamePlayer = optionalGamePlayer.get();
+
+        actions.forEach(action -> {
+            final String[] split = action.split(";");
+            final String actionId = split[0];
+            final Action actionObject = actionId == null ? null : this.actions.get(actionId.toLowerCase());
+
+            if (actionObject == null) {
+                plugin.getLogger().warning("Action with ID '" + actionId + "' not found.");
+                return;
+            }
+
+            String data = action.length() > 1 ? action.substring(action.length() - 1) : "";
+            // TODO PAPI
+
+            actionObject.onAction(plugin, gamePlayer, data);
+        });
+    }
+
+}
